310c535ad90586dbc064bbc8f8f9e13a485c2bc7
furkanaydar <furkanaydar97@gmail.com>
2020-05-29 21:34:14 +0300

    Created Item entity and db connection

---
+++ src/main/java/com/shopping/project/model/Item.java
@@ -0,0 +1,48 @@
+package com.shopping.project.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String itemType;
+    private Double price;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/shopping/project/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.shopping.project.repositories;
+
+import com.shopping.project.model.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
+}
