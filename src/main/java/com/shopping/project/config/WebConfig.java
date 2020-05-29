b846e5a2dd1482843017c3af104bb524c4cfaf95
furkanaydar <furkanaydar97@gmail.com>
2020-05-29 23:30:21 +0300

    Init CORS support

--- src/main/java/com/shopping/project/config/WebConfig.java
+++ src/main/java/com/shopping/project/config/WebConfig.java
@@ -7,6 +7,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("http://shopping.herokuapp.com");
     }
 }
