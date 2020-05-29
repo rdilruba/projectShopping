727ba1098093378647d32a7f7f4fc7bd9bdb9ca3
furkanaydar <furkanaydar97@gmail.com>
2020-05-29 23:28:34 +0300

    Init CORS support

---
+++ src/main/java/com/shopping/project/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.shopping.project.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
