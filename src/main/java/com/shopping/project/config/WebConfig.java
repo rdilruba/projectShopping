76e195616f06ce4701a0987c9fa1fdc4fe1d3164
furkanaydar <furkanaydar97@gmail.com>
2020-05-31 19:58:19 +0300

    fix cors

--- src/main/java/com/shopping/project/config/WebConfig.java
+++ src/main/java/com/shopping/project/config/WebConfig.java
@@ -1,12 +1,16 @@
 package com.shopping.project.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://shopping.herokuapp.com");
+        registry.addMapping("/**").allowedOrigins("*").maxAge(3600)
+                .allowedMethods("GET", "POST","PUT", "DELETE").allowedHeaders("*");
+
     }
 }
