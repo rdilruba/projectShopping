dc30e2d477a90fa8cc2f87d2e030373bf2c04eda
furkanaydar <furkanaydar97@gmail.com>
2020-06-02 20:46:57 +0300

    delete product endpoint

--- src/main/java/com/shopping/project/services/impl/ProductServiceImpl.java
+++ src/main/java/com/shopping/project/services/impl/ProductServiceImpl.java
@@ -40,6 +40,6 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public void deleteProduct(Long id) {
-        productRepository.deleteProductById(id);
+        productRepository.deleteById(id);
     }
 }
